0fb7a024432b1c6375a7887a2cca671f22b0c6af
SantiagoDiVirgilio <135436329+SantiagoDiVirgilio@users.noreply.github.com>
2025-10-12 22:15:30 -0300

    .

---
+++ contacto.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<link rel="icon" href="imagenes/favicon.webp" type="image/webp">
+	<title>Sistema de Gestion de Sociedad de Fomento</title>
+	<meta charset="UTF-8">
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+	</style>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+
+<body>
+	<header>
+		<a href="index.php"><img src="imagenes/logo.webp" alt="Logo de la página" class="logo"></a>
+	
+	<?php
+    include("NAV.php");
+    ?>
+
+	<div class="mobile-header-bar">
+	<a href="javascript:void(0);" class="icon" onclick="toggleMenu()">&#9776;</a>
+	</header>
+
+	<article >
+    <section class="formu">
+        <h3>CONTACTO</h3><br>
+        <p>¿Quiere hacer algún comentario en cuanto a nuestra página web?<br>
+                  ¡Todos sus mensajes son bienvenidos!<br>
+            ¡Complete el formulario y envíelo!
+        </p>
+        <form action="contacto.php" method="post">
+            <br><label for="nom">Nombre:</label>
+            <input class="input_formu" type="text" name="nombre" maxlength="20" id="identificador"; width:150px>
+            <br>
+            <br><label for="email">Email:</label>
+            <input class="input_formu" type="email" name="email" maxlength="40" id="identificador"; width:150px>
+            <br>
+            <br><label for="comentario">Haga su comentario <strong>aquí:</strong></label><br><br>
+            <textarea class="input_formu" arows="400" cols="60" maxlength="700" style="width: 645px; height: 128px;" name="comentario"> 
+            </textarea>
+            <br>
+            <br><input type="submit" value="Enviar" id="identificador">
+            <input type="reset" value="Resetear Información" id="identificador">
+        </form>
+    </section>
+</article>
+	
+<footer>
+<?php
+    include("FOOTER.php");
+?>
+</footer>
+
+<!-- Script de efecto zoom -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+    $('.redes').hover(function() {
+        $(this).addClass('transition');
+    }, function() {
+        $(this).removeClass('transition');
+    });
+});
+
+function toggleMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className.includes("responsive")) {
+    x.className = "";
+  } else {
+    x.className += " responsive";
+  }
+}
+</script>
+</body>
+
+</html>
